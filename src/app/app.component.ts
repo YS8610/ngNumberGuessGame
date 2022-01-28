301eeeceeec85943ae218fdd7ad3e8e739bdb634
YS_workPC <ong_ys86@yahoo.com.sg>
2022-01-28 14:15:54 +0800

    done without css styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngNumberGuessGame';
+
+  minimum = 0;
+  maximum = 100;
+  randomNumber = Math.floor(Math.random() * (this.maximum-1)) + (this.minimum+1);
+  isCompleted = false;
+  msg = "";
+  guesses: number[] = [];
+
+  userinput = new FormControl('');
+
+
+  onSubmit(){
+    console.log(this.userinput.value);
+    this.guesses.push(this.userinput.value);
+    if (this.userinput.value == this.randomNumber ){
+      this.msg = "You guessed the right number... Please refresh the browser to restart the game";
+      this.isCompleted = true;
+    }
+    else if ( this.userinput.value > this.randomNumber){
+      this.maximum = this.userinput.value;
+      this.msg = "The range is from " + (this.minimum+1) + " to " + (this.maximum-1);
+    }
+    else{
+      this.minimum = this.userinput.value;
+      this.msg = "The range is from " + (this.minimum+1) + " to " + (this.maximum-1);
+    }
+  }
 }
