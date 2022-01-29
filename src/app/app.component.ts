c1f10709ba77fde23ce1afe9baf103d16a0e184f
YS_workPC <ong_ys86@yahoo.com.sg>
2022-01-29 19:41:15 +0800

    fix bug for prevent entry invalid number pressing enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,23 +16,26 @@ export class AppComponent {
   msg = "";
   guesses: number[] = [];
 
-  userinput = new FormControl('');
+  userinput = new FormControl('',Validators.required);
 
 
   onSubmit(){
     console.log(this.userinput.value);
-    this.guesses.push(this.userinput.value);
-    if (this.userinput.value == this.randomNumber ){
-      this.msg = "You guessed the right number... Please refresh the browser to restart the game";
-      this.isCompleted = true;
-    }
-    else if ( this.userinput.value > this.randomNumber){
-      this.maximum = this.userinput.value;
-      this.msg = "The range is from " + (this.minimum+1) + " to " + (this.maximum-1);
-    }
-    else{
-      this.minimum = this.userinput.value;
-      this.msg = "The range is from " + (this.minimum+1) + " to " + (this.maximum-1);
+    if (this.userinput.value>this.minimum && this.userinput.value< this.maximum){
+      this.guesses.push(this.userinput.value);
+
+      if (this.userinput.value == this.randomNumber ){
+        this.msg = "You guessed the right number... Please refresh the browser to restart the game";
+        this.isCompleted = true;
+      }
+      else if ( this.userinput.value > this.randomNumber){
+        this.maximum = this.userinput.value;
+        this.msg = "The range is from " + (this.minimum+1) + " to " + (this.maximum-1);
+      }
+      else{
+        this.minimum = this.userinput.value;
+        this.msg = "The range is from " + (this.minimum+1) + " to " + (this.maximum-1);
+      }
     }
   }
 }
